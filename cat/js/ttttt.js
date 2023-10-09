b249450a82e02cede11377a8fa4bc4a54e26359a
laoZ <71415980+xyzjhe@users.noreply.github.com>
2023-10-10 00:53:30 +0800

    Update ttttt.js

--- cat/js/ttttt.js
+++ cat/js/ttttt.js
@@ -2,7 +2,7 @@ export var config = {
 
 	自定义分类:0;
         
-        cookie = "http://127.0.0.1:9978/file/tvbox/bilicookie.txt"
+       loc_conf.cookie = "http://127.0.0.1:9978/file/tvbox/bilicookie.txt"
 	
 
 	
