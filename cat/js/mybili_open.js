293ce316894e328b60fbe8535a72f18d6039e26f
laoZ <71415980+xyzjhe@users.noreply.github.com>
2023-10-14 10:33:18 +0800

    Update mybili_open.js

--- cat/js/mybili_open.js
+++ cat/js/mybili_open.js
@@ -1,12 +1,12 @@
 // 一级&搜索页 直向改横向 --> land:1, ratio:1.78,
-import { _ } from "assets://js/lib/cat.js";
+import { _ } from "./js/lib/cat.js";
 let key = '我的哔哩';
 let HOST = 'https://api.bilibili.com';
 let siteKey = '';
 let siteType = 0;
 const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
 // let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
-let cookie = "填自己的bili cookie"; // 可更换成自己的cookie
+let cookie = "http://127.0.0.1:9978/file/tvbox/bilicookie.txt"; // 可更换成自己的cookie
 
 async function request(reqUrl) {
   const res = await req(reqUrl, {
