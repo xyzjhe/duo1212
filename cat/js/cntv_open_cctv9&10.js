53e56ea34e9edbd17a1ef03cd8329fcb18ce1cc0
laoZ <71415980+xyzjhe@users.noreply.github.com>
2023-10-17 11:59:17 +0800

    Update cntv_open_cctv9&10.js

--- cat/js/cntv_open_cctv9&10.js
+++ cat/js/cntv_open_cctv9&10.js
@@ -24,27 +24,27 @@ async function init(cfg) {
 async function home(filter) {
 	const classes = [
 	{type_id: "TOPC1460958142363297", type_name: "纪录频道"},
-	{type_id: "TOPC1571389841186799", type_name: "纪录频道-微9"},
+	{type_id: "TOPC1571389841186799", type_name: "纪录频道·微9"},
 	{type_id: "TOPC1451557052519584", type_name: "百家讲坛 "},
 	{type_id: "TOPC1451557421544786", type_name: "地理·中国"},
-{type_id: "TOPC1451558150787467", type_name: "自然传奇"},
-{type_id: "TOPC1482483166133803", type_name: "味道"},
-{type_id: "TOPC1451557646802924", type_name: "健康之路"},
-{type_id: "TOPC1451557893544236", type_name: "探索·发现"},
-   {type_id: "TOPC1576631973420833", type_name: "透视新科技"},
- {type_id: "TOPC1571021159595290", type_name: "实验现场"},
-    {type_id: "TOPC1571021323137369", type_name: "科幻地带"},
-{type_id: "TOPC1571021385508957", type_name: "科学动物园"},
-  {type_id: "TOPC1570874587435537", type_name: "时尚科技秀"},
-{type_id: "TOPC1570780618796536", type_name: "人物·故事"},
-  {type_id: "TOPC1571021251454875", type_name: "考古公开课"},
-{type_id: "TOPC1575253587571324", type_name: "跟着书本去旅行"},
-    {type_id: "TOPC1574995326079121", type_name: "百家说故事"},
-{type_id: "TOPC1451557523542854", type_name: "读书"},
-  {type_id: "TOPC1570876640457386", type_name: "解码科技史"}
-  {type_id: "TOPC1570875218228998", type_name: "创新进行时"}
-          {type_id: "TOPC1592552941644815",type_name: "中国影像方志"}
-];
+        {type_id: "TOPC1451558150787467", type_name: "自然传奇"},
+        {type_id: "TOPC1482483166133803", type_name: "味道"},
+        {type_id: "TOPC1451557646802924", type_name: "健康之路"},
+        {type_id: "TOPC1451557893544236", type_name: "探索·发现"},
+        {type_id: "TOPC1576631973420833", type_name: "透视新科技"},
+        {type_id: "TOPC1571021159595290", type_name: "实验现场"},
+        {type_id: "TOPC1571021323137369", type_name: "科幻地带"},
+        {type_id: "TOPC1571021385508957", type_name: "科学动物园"},
+        {type_id: "TOPC1570874587435537", type_name: "时尚科技秀"},
+        {type_id: "TOPC1570780618796536", type_name: "人物·故事"},
+        {type_id: "TOPC1571021251454875", type_name: "考古公开课"},
+        {type_id: "TOPC1575253587571324", type_name: "跟着书本去旅行"},
+        {type_id: "TOPC1574995326079121", type_name: "百家说故事"},
+        {type_id: "TOPC1451557523542854", type_name: "读书"},
+        {type_id: "TOPC1570876640457386", type_name: "解码科技史"}
+        {type_id: "TOPC1570875218228998", type_name: "创新进行时"}
+        {type_id: "TOPC1592552941644815",type_name: "中国影像方志"}
+      ];
 	const filterObj = {};
 	return JSON.stringify({
 		class: _.map(classes, (cls) => {
